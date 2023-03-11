168f4e5a17f93810fd07f767015a568343c9a6d6
whiskacz <pawelblasiak1985@gmail.com>
2023-03-11 12:39:28 +0100

    CRA exercise 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const data = [
+  {id: 1, title: "Wiadomość numer 1", body: "Zawartość wiadomości numer 1 ... "},
+  {id: 2, title: "Wiadomość numer 2", body: "Zawartość wiadomości numer 2 ... "},
+]
+setInterval(()=>{
+  const index = data.length +1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości numer ${index}`
+    
+  })
+  
+},2000)
+
+class App extends Component { 
+  state = {
+    comments : [...data]
+  }
+  getData = () => {
+    if(this.state.comments.length !== data.length){
+    this.setState({
+      comments: [...data]
+    })
+  } else{console.log('dane takie same - nie aktualizuje')}    
+  }
+  componentDidMount(){
+   this.idI = setInterval(this.getData, 1000)
+  }
+  componentWillUnmount(){
+    clearInterval(this.idI)
+  }
+
+
   render() {
+    const comments = this.state.comments.map(comment =>(
+    <div key={comment.id}>
+      <h4>{comment.title}</h4>
+      <div>{comment.body}</div>
+    </div>
+    ))
+   
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div>{comments.reverse()}</div>
       </div>
     );
   }
